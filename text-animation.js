0d7d07f0636c4a4ca0876e389abe2e98b3c2504f
UjjwalBaranwal <127297699+UjjwalBaranwal@users.noreply.github.com>
2023-03-20 14:31:50 +0530

    Add files via upload

---
+++ text-animation.js
@@ -0,0 +1,50 @@
+function typingEffect() {
+  const contactTexts = shuffleArray([
+    "Ask us anything!",
+    "Want to talk about an idea?",
+    "Lets do something together!",
+    "Do you need help in something?",
+    "Contact us for making your projects"
+  ]);
+  const typedtext = document.getElementsByClassName("typedtext")[0];
+  let removing = false;
+  let idx = (char = 0);
+
+  setInterval(() => {
+    if (char < contactTexts[idx].length)
+      typedtext.innerHTML += contactTexts[idx][char];
+
+    if (char == contactTexts[idx].length + 15) removing = true;
+
+    if (removing)
+      typedtext.innerHTML = typedtext.innerHTML.substring(
+        0,
+        typedtext.innerHTML.length - 1
+      );
+
+    char++;
+    if (typedtext.innerHTML.length === 0) {
+      if (idx === contactTexts.length - 1) idx = 0;
+      else idx++;
+
+      char = 0;
+      removing = false;
+    }
+  }, 100);
+}
+typingEffect();
+function shuffleArray(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
